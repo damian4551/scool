bd4a8c99302d6f1ea7ddf5b125ba8baa574d99ee
Damian <damian@MacBook-Air-Damian.local>
2021-02-03 14:42:41 +0100

    database configuration

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5553cfb8c0b4ddf252404e79973608d2f92539a0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5553cfb8c0b4ddf252404e79973608d2f92539a0',
+    ),
+  ),
+);
